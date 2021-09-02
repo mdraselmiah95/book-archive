4384ecaa86ce56d7f1c97c0448ee991e23d477e0
rasel <russelroy15@gmail.com>
2021-09-02 08:38:51 +0600

    final touch

--- js/app.js
+++ js/app.js
@@ -4,17 +4,16 @@ const bookContainer = document.getElementById("book-container");
 const totalResult = document.getElementById("result");
 const showError = document.getElementById("error");
 
-
 searchBtn.addEventListener("click", function () {
   const searchText = searchInput.value;
   if (searchText === "") {
     showError.innerText = "Search field cannot be empty.";
     return;
-    }
-    //clear data
-    searchInput.value = "";
-    bookContainer.innerHTML = "";
-    //fetch url
+  }
+  //clear data
+  searchInput.value = "";
+  bookContainer.innerHTML = "";
+  //fetch url
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
   fetch(url)
     .then((res) => res.json())
@@ -22,27 +21,34 @@ searchBtn.addEventListener("click", function () {
 });
 
 const displayBooks = (books) => {
-    //Error handle
-    if (books.length === 0) {
-        alert("No result found.")
-    }
-        showError.innerText = "";
-    books.forEach(book => {
-        console.log(book.numFound)
-        // totalResult.innerText = book.length;
-            const bookCover = ` https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-            const div = document.createElement("div");
-            div.classList.add("col");
-            div.innerHTML = `
+  //Total search result
+  const totalResultFound = books.length;
+  totalResult.innerText = `Total search Result: ${totalResultFound}`;
+  //Error handle
+  if (books.length === 0) {
+    alert("No result found.");
+  }
+  showError.innerText = "";
+  books.forEach((book) => {
+    const bookCover = ` https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
             <div class="card p-2">
-                <img src="${bookCover ? bookCover : ''}" class="card-img-top img-fluid" alt="country flag" />
+                <img src="${
+                  bookCover ? bookCover : ""
+                }" class="card-img-top img-fluid" alt="country flag" />
                 <div class="card-body">
-                    <h3 class="card-title text-success fw-bold">${book.title}</h3>
-                    <h4 class="text-secondary fw-bold">${book.author_name}</h4>
-                    <h5 class="fw-bold">Publisher: ${book.publisher}</h5>
-                    <p class="">First publish year: ${book.first_publish_year}</p>
+                    <h4 class="card-title fw-bold text-dark">${book.title}</h4>
+                    <p class="text-danger fst-italic">${book.author_name}</p>
+                    <h5 class="text-secondary">Publisher:<span class="text-dark"> ${
+                      book.publisher
+                    }</span></h5>
+                    <p class="text-secondary">First publish year: <span class="fw-bold fs-4 text-dark">${
+                      book.first_publish_year
+                    }</span></p>
                 </div>
             </div> `;
-            bookContainer.appendChild(div)
-        });
-}
+    bookContainer.appendChild(div);
+  });
+};
